8edba3038ed6b345376852b7e761484e671d9d27
Roman Jimenez <drrouman@gmail.com>
2011-12-17 21:05:59 +0100

    Additional artwork done (even sound!)

--- src/game.py
+++ src/game.py
@@ -65,6 +65,7 @@ class Guy():
         self.original_image = self.image
         self.rect = self.image.get_rect()
         self.rect.move_ip(RESOLUTION[0] / 2, RESOLUTION[1] / 2)
+        self.radius = 32
     
         self.speed = 3
         self.rotateSpeed = 5
@@ -102,21 +103,34 @@ def getCrosshairPosition(playerPosition):
 
 def main():
     pygame.init()
+    pygame.mixer.init()
     main_surface = pygame.display.set_mode(RESOLUTION)
+    badguy = load_image('res/badguy.png')
+    helper = load_image('res/helper.png')
+    origbadguy = badguy
+    orighelper = helper
+    background_image = load_image('res/background.png', False)
     pygame.display.set_caption(TITLE)
     pygame.key.set_repeat(KEY_DELAY, KEY_INTERVAL)
     fpsTimer = pygame.time.Clock()
 #    pygame.mouse.set_visible(False)
     guy = Guy()
     bullet = Bullet(guy)
+    sndindex = 0
+    bulletsnds = [pygame.mixer.Sound('res/shot1.wav'), pygame.mixer.Sound('res/shot2.wav')] 
     while True:
         # Render code 
-        main_surface.fill(WHITE)
+        main_surface.blit(background_image, (0,0))
+        pygame.draw.circle(main_surface, (255, 210, 0), guy.rect.center, guy.radius)
         main_surface.blit(bullet.image, bullet.rect)
-        main_surface.blit(guy.image, guy.rect)
+        main_surface.blit(guy.image, guy.rect)  
+        main_surface.blit(badguy, (50,50))
+        main_surface.blit(helper, (500,500))
         
         # Update code
         bullet.update()
+        badguy = pygame.transform.rotate(origbadguy, guy.rotated)
+        helper = pygame.transform.rotate(orighelper, guy.rotated)
         
         # Has to be called outside, could be called multiple times
         guy.moveGuy(pygame.key.get_pressed())
@@ -132,6 +146,12 @@ def main():
                     exit()
                 if evt.key == K_SPACE:
                     bullet.fire()
+                    bulletsnds[sndindex].stop()
+                    sndindex += 1
+                    bulletsnds[sndindex].play()
+                    if (sndindex == 1):
+                        sndindex = 0
+                    guy.radius += 1
 
         pygame.display.update()
         fpsTimer.tick(TARGET_FPS)
